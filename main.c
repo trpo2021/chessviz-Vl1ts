d99268ba79f2ccf00abb386ca144885c21ff9be5
Vl1ts <vladooooos111@yandex.ru>
2021-02-19 18:30:06 -0500

    Remake the  program way adding the structure and therefore the pointers for this struct to fix the issue with chessboard's output

--- main.c
+++ main.c
@@ -20,39 +20,41 @@ typedef enum {
     EmptyZone = ' ',
 } Figures;
 
-void CreateBoard()
-{
-	Figures Board[8][8];	
+typedef struct{			//struct Board_Struct{
+    Figures Board[8][8];	//Figures Board[8][8];
+} Board_Struct;			//};
+
+void CreateBoard(Board_Struct *board_pointer)
+{	
+	board_pointer->Board[7][0]=WRook;
+	board_pointer->Board[7][1]=WKnight;
+	board_pointer->Board[7][2]=WBishop;
+	board_pointer->Board[7][3]=WQueen;
+	board_pointer->Board[7][4]=WKing;
+	board_pointer->Board[7][5]=WBishop;
+	board_pointer->Board[7][6]=WKnight;
+	board_pointer->Board[7][7]=WRook;
 	
-	Board[7][0]=WRook;
-	Board[7][1]=WKnight;
-	Board[7][2]=WBishop;
-	Board[7][3]=WQueen;
-	Board[7][4]=WKing;
-	Board[7][5]=WBishop;
-	Board[7][6]=WKnight;
-	Board[7][7]=WRook;
-	
-	Board[0][0]=BRook;
-	Board[0][1]=BKnight;
-	Board[0][2]=BBishop;
-	Board[0][3]=BQueen;
-	Board[0][4]=BKing;
-	Board[0][5]=BBishop;
-	Board[0][6]=BKnight;
-	Board[0][7]=BRook;
+	board_pointer->Board[0][0]=BRook;
+	board_pointer->Board[0][1]=BKnight;
+	board_pointer->Board[0][2]=BBishop;
+	board_pointer->Board[0][3]=BQueen;
+	board_pointer->Board[0][4]=BKing;
+	board_pointer->Board[0][5]=BBishop;
+	board_pointer->Board[0][6]=BKnight;
+	board_pointer->Board[0][7]=BRook;
 	
 	for (int i=0; i<BoardLineSize; ++i)
 	{
-		Board[6][i]=WPawn;
-		Board[1][i]=BPawn;
+		board_pointer->Board[6][i]=WPawn;
+		board_pointer->Board[1][i]=BPawn;
 	}
 	for (int i=2; i<BoardLineSize-2; ++i)
 		for (int j=0; j<BoardLineSize; ++j)
-			Board[i][j]=EmptyZone;
+			board_pointer->Board[i][j]=EmptyZone;
 }
 
-void PrintBoard(Figures Board)
+void PrintBoard(Board_Struct *board_pointer)
 {
 	int LineNumber=8;
 	for (int i=0; i<BoardLineSize; ++i)
@@ -60,7 +62,7 @@ void PrintBoard(Figures Board)
 		printf("%d", LineNumber);
 		LineNumber--;
 		for (int j=0; j<BoardLineSize; ++j)
-			printf(" %c", Board[i][j]);
+			printf(" %c", board_pointer->Board[i][j]);
 		printf("\n");
 	}
 	printf("  a b c d e f g h\n");
@@ -68,7 +70,7 @@ void PrintBoard(Figures Board)
 
 int main() 
 {
-	Figures Board;
-	CreateBoard();
-	PrintBoard(Board);
+	Board_Struct Board;		//struct Board_Struct Board;
+	CreateBoard(&Board);
+	PrintBoard(&Board);
 }
