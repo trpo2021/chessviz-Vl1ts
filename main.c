0b801044bb05de2e4b450b6bca6d08aa2e3346da
Vl1ts <vladooooos111@yandex.ru>
2021-02-19 17:13:53 -0500

    Add function filling the start chessboard

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+const int BoardLineSize=8;
+
 typedef enum {    
     WKing = 'K',
     WQueen = 'Q',
@@ -15,6 +17,38 @@ typedef enum {
     
     Empty = ' ',
 } Figures;
+
+void CreateBoard()
+{
+	Figures Board[8][8];
+	
+	Board[7][0]=WRook;
+	Board[7][1]=WKnight;
+	Board[7][2]=WBishop;
+	Board[7][3]=WQueen;
+	Board[7][4]=WKing;
+	Board[7][5]=WBishop;
+	Board[7][6]=WKnight;
+	Board[7][7]=WRook;
+	
+	Board[0][0]=BRook;
+	Board[0][1]=BKnight;
+	Board[0][2]=BBishop;
+	Board[0][3]=BQueen;
+	Board[0][4]=BKing;
+	Board[0][5]=BBishop;
+	Board[0][6]=BKnight;
+	Board[0][7]=BRook;
+	
+	for (int i=0; i<BoardLineSize; ++i)
+	{
+		Board[6][i]=WPawn;
+		Board[1][i]-BPawn;
+	}
+	for (int i=2; i<BoardLineSize-3; ++i)
+		for (int j=0; j<BoardLineSize; ++j)
+			Board[i][j]=Empty;
+}
 
 int main() 
 {
