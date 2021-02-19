b756b36f7dfda95dd083c0091c7c3455ec5b9ddc
Vl1ts <vladooooos111@yandex.ru>
2021-02-19 16:46:06 -0500

    Create enumeration of possible figures

---
+++ main.c
@@ -0,0 +1,22 @@
+typedef enum {    
+    WKing = 'K',
+    WQueen = 'Q',
+    WRook = 'R',
+    WKnight = 'N',
+    WBishop = 'B',
+    WPawn = 'P',
+    
+    BKing = 'k',
+    BQueen = 'q',
+    BRook = 'r',
+    BKnight = 'n',
+    BBishop = 'b',
+    BPawn = 'p',
+    
+    Empty = ' ',
+} Figures;
+
+int main() 
+{
+
+}
