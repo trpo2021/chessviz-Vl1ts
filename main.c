c492319d22fb2c0678756ca2331c13387e1d9b18
Vl1ts <vladooooos111@yandex.ru>
2021-02-28 14:01:35 -0500

    Remake the program: enum, struct -> char array

--- main.c
+++ main.c
@@ -1,74 +1,54 @@
 #include <stdio.h>
 
-const int board_line_size = 8;
+const int board_line_size=8;
 
-typedef enum {
-    wKing = 'K',
-    wQueen = 'Q',
-    wRook = 'R',
-    wKnight = 'N',
-    wBishop = 'B',
-    wPawn = 'P',
-
-    bKing = 'k',
-    bQueen = 'q',
-    bRook = 'r',
-    bKnight = 'n',
-    bBishop = 'b',
-    bPawn = 'p',
-
-    empty_zone = ' ',
-} Figures;
-
-typedef struct {         // struct Board_Struct{
-    Figures Board[8][8]; // Figures Board[8][8];
-} Board_Struct;          //};
-
-void create_board(Board_Struct* board_pointer)
-{
-    board_pointer->Board[7][0] = wRook;
-    board_pointer->Board[7][1] = wKnight;
-    board_pointer->Board[7][2] = wBishop;
-    board_pointer->Board[7][3] = wQueen;
-    board_pointer->Board[7][4] = wKing;
-    board_pointer->Board[7][5] = wBishop;
-    board_pointer->Board[7][6] = wKnight;
-    board_pointer->Board[7][7] = wRook;
-
-    board_pointer->Board[0][0] = bRook;
-    board_pointer->Board[0][1] = bKnight;
-    board_pointer->Board[0][2] = bBishop;
-    board_pointer->Board[0][3] = bQueen;
-    board_pointer->Board[0][4] = bKing;
-    board_pointer->Board[0][5] = bBishop;
-    board_pointer->Board[0][6] = bKnight;
-    board_pointer->Board[0][7] = bRook;
-
-    for (int i = 0; i < board_line_size; ++i) {
-        board_pointer->Board[6][i] = wPawn;
-        board_pointer->Board[1][i] = bPawn;
-    }
-    for (int i = 2; i < board_line_size - 2; ++i)
-        for (int j = 0; j < board_line_size; ++j)
-            board_pointer->Board[i][j] = empty_zone;
+void create_board(char board[board_line_size][board_line_size])
+{	
+	board[7][0]='R';
+	board[7][1]='N';
+	board[7][2]='B';
+	board[7][3]='Q';
+	board[7][4]='K';
+	board[7][5]='B';
+	board[7][6]='N';
+	board[7][7]='R';
+	
+	board[0][0]='r';
+	board[0][1]='n';
+	board[0][2]='b';
+	board[0][3]='q';
+	board[0][4]='k';
+	board[0][5]='b';
+	board[0][6]='n';
+	board[0][7]='r';
+	
+	for (int i=0; i<board_line_size; ++i)
+	{
+		board[6][i]='P';
+		board[1][i]='p';
+	}
+	for (int i=2; i<board_line_size-2; ++i)
+		for (int j=0; j<board_line_size; ++j)
+			board[i][j]=' ';
 }
 
-void print_board(Board_Struct* board_pointer)
+void print_board(char board[board_line_size][board_line_size])
 {
-    int line_number = 8;
-    for (int i = 0; i < board_line_size; ++i) {
-        printf("%d", line_number);
-        line_number--;
-        for (int j = 0; j < board_line_size; ++j)
-            printf(" %c", board_pointer->Board[i][j]);
-        printf("\n");
-    }
-    printf("  a b c d e f g h\n");
+	int line_number=8;
+	for (int i=0; i<board_line_size; ++i)
+	{
+		printf("%d", line_number);
+		line_number--;
+		for (int j=0; j<board_line_size; ++j)
+			printf(" %c", board[i][j]);
+		printf("\n");
+	}
+	printf("  a b c d e f g h\n");
 }
 
-int main()
+int main() 
 {
-    Board_Struct Board; // struct Board_Struct Board;
-    create_board(&Board);
-    print_board(&Board);
+	char chessboard[board_line_size][board_line_size];
+	create_board(chessboard);
+	print_board(chessboard);
 }
